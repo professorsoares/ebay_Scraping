1e5895c27096c95d7f5852c2cafcf90181a45f74
unknown <professorsoares@gmail.com>
2022-07-17 23:29:45 -0300

    Scrapping funcionando!

--- ebay_scraper_ListaRelogios.py
+++ ebay_scraper_ListaRelogios.py
@@ -58,40 +58,47 @@ def get_detail_data(soup):
 
 def get_index_data(soup):
 	try:
-		#product_name = soup.find_all('h3', class_="s-item__title")
-		#product_name = soup.find_all('div', class_="s-item__info clearfix")
+		bloco_produtos = soup.find('div', id='srp-river-results')
+	except:
+		bloco_produtos = ""
 
-		links = soup.find_all("a", {"class": "s-item__link"})
 
+	try:
+		if bloco_produtos  != "":
+			links = bloco_produtos.find_all("a", {"class": "s-item__link"})
+		else:
+			links = []
 	except:
 		links = []
 
-	try:
-		products = soup.find_all('div', class_="s-item__wrapper clearfix")
-	except:
-		products = []
-
 	urls = [item.get('href') for item in links]
-	print(len(links))
+
+	print(urls)
 	print('1: -------------------------------------------')
 
-	product_name = [item.find('h3') for item in products]
+	# try:
+	# 	products = soup.find_all('div', class_="s-item__wrapper clearfix")
+	# except:
+	# 	products = []
+	#
+	# product_name = [item.find('h3') for item in products]
+	#
+	# print(product_name[0].text)
 
-	print(product_name[0].text)
-
+	return urls
 
 def main():
-	url = 'https://www.ebay.com/itm/174966171523?epid=28020141486&hash=item28bccbe783:g:81cAAOSwC7JhXMmD&amdata=enc%3AAQAHAAAA8GoR7i%2Bib2yNGNP5h9to61anQ3jlclrDEp6IYhIwB52KkqYxzHQOuFTRb2tVHUnZAz%2FAZXnko9Mi6K1Imwo6TTSIlEi1gsEQTx27TKPdrencmbal2rO4p6U9gKwVEZ8qRClD6rk2G0VtjtxFLPGob90sbqZ23cbuHjgkCUOPrjMDw2L00BS0ZaqCyomrBxSj6Mo43djPj88tiulfQaFupPDR2Y9o83mSnYeGeEwfC1zXGA3wdVQnnfj7yk8XvZy%2BtN86gmphhVkyMuBN6HCP8WZs1IZqJOJPS88cDIx8%2F2xIyV8%2BF60dHCOqqgZnm64KCg%3D%3D%7Ctkp%3ABFBM_u3cx8Fg' # Link of detail page
-	soup = get_page(url)
-	get_detail_data(soup)
 
-	print('0: -------------------------------------------')
-
-	# Segunda chamada: Lista de relógios:
+	# Busca a lista de Produtos
 	url_lista_relogios = 'https://www.ebay.com/sch/i.html?_nkw=watches+for+men&_pgn=2'
 	soup = get_page(url_lista_relogios)
-	get_index_data(soup)
-	
+	products = get_index_data(soup)
+
+	# Pega o link de cada produto para buscar os dados desse produto
+	for link in products:
+		print("LINK: " + link)
+		data = get_detail_data(get_page(link))
+		print(data)
 	
 if __name__ == '__main__':
 	main()
